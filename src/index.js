375adbde4fe9466a3abe05d83f118473ba5af085
Vxee <15258691200@163.com>
2018-05-17 20:43:09 +0800

    管理输出

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import _ from 'lodash';
+import printMe from './print.js';
 
 function component() {
     var element = document.createElement('div');
+    var button = document.createElement('button');
   
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+    
+    button.innerHTML = 'click me and check the console!';
+    button.onclick = printMe;
+    element.appendChild(button);
+
     return element;
-  }
+}
   
-  document.body.appendChild(component());
+document.body.appendChild(component());
